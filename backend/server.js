a4a3dfea873d6a9ff9574d152f39b073ee003197
Spencer Heywood <spencer@bookservo.com>
2026-04-07 09:14:15 -0400

    fix duplicate fs require statement

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
